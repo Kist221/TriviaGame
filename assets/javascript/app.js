b06dab11e2958f794be6f0025c433060d37ee015
Kist221 <Kist221@gmail.com>
2017-11-25 15:28:17 -0700

    runtime logic in place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,17 +25,27 @@ var incorrect = 0;
 var current = 0;
 
 // number of questions skipped or missed
-var blank = questions.length;
+var remaining = questions.length;
 
 
 
 // game functions
+
+// reset variables
+var reset = function ()
+{
+	correct = 0;
+	incorrect = 0;
+	current = 0;
+	remaining = questions.length;
+};
 
 // starts game when player clicks start
 var startGame = function ()
 {
 	$("#start").on("click", function()
 	{
+		reset();
 		popQuestion();
 		$("#start").hide();
 		answering();
@@ -47,42 +57,57 @@ var popQuestion = function ()
 {
 	// list questions on page
 	$("#questions").append("<h2>" + questions[current].q + "</h2>");
-
 	// loop length of answer options
 	for (var j = 0; j < questions[current].a.length; j++)
 	{
 		// make var with answers
 		var answer = $("<button>" + questions[current].a[j].a + "</button>");
-
 		// add class for referencing click functions
 		answer.addClass("answer");
-
 		// store data whether answer is T or F
 		answer.attr("data", questions[current].a[j].correct);
-		
 		// list answers on page
 		$("#questions").append(answer);
 	}
+	// change current question value
+	current++;
+	// update remaining questions
+	remaining--;
 };
 
+// populate next question
+var nextQuestion = function ()
+{
+	// clear question container for next question
+	$("#questions").empty();
+	// populate next question
+	popQuestion();
+};
+
+// function for determining correct answer and displaying next question
 var answering = function ()
 {
-	$(".answer").on("click", function ()
+	// run when an answer is selected
+	$("#questions").on("click", ".answer", function ()
 	{
-		console.log($(this).attr("data"));
+		// if answer is correct
 		if ($(this).attr("data") === "true") {
-			console.log("true");
-		} else {
-			console.log("false");
+			// store correct input 
+			correct++;
 		}
+		// if answer is incorrect
+		else {
+			// store incorrect input
+			incorrect++;
+		}
+		// NEXT QUESTION
+		nextQuestion();
 	})
 };
 
 
 
-
 startGame();
-
 
 
 
